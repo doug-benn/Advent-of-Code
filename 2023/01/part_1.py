7e6114a7b89b97674fb5428f5ee8eb9ab2fe2417
doug-benn <fiery.coder@gmail.com>
2023-12-13 21:45:03 +0000

    Day 1 Part 2 Solved

--- 2023/01/part_1.py
+++ 2023/01/part_1.py
@@ -12,9 +12,24 @@ def find_last(str):
 
 numbers = []
 
-with open("./2023/01/part_1_input.txt") as file:
+number_names = [
+    ("one", "one1one"),
+    ("two", "two2two"),
+    ("three", "three3three"),
+    ("four", "four4four"),
+    ("five", "five5five"),
+    ("six", "six6six"),
+    ("seven", "seven7seven"),
+    ("eight", "eight8eight"),
+    ("nine", "nine9nine"),
+]
+
+
+with open("./2023/01/input.txt") as file:
     lines = file.readlines()
     for line in lines:
+        for pair in number_names:
+            line = line.replace(pair[0], pair[1])
         first = find_first(line.split())
         last = find_last(line.strip())
         result = first + last
